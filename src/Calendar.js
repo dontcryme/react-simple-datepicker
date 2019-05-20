72b89ecfd186067f916837300d14d5aae9b2935d
Dongjin Kim <dontcryme@naver.com>
2019-05-20 13:24:46 +0900

    Update Calendar.js
    fix : Uncaught TypeError: Cannot read property 'instanceOf' of undefined

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,146 +1,184 @@
-import React, { Component, PropTypes } from 'react';
-import Day from './Day';
-import {
-  daysOfMonth,
-  weekEnum,
-  isDateFromNextMonth,
-  isDateFromPrevMonth
-} from './utils/';
-import moment from 'moment';
+'use strict';
 
-export default class Calendar extends Component {
-  constructor (props) {
-    super(props);
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var PropTypes = require('prop-types');
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+var _DateInput = require('./DateInput');
+
+var _DateInput2 = _interopRequireDefault(_DateInput);
+
+var _Calendar = require('./Calendar');
+
+var _Calendar2 = _interopRequireDefault(_Calendar);
+
+var DatePicker = (function (_Component) {
+  _inherits(DatePicker, _Component);
+
+  function DatePicker(props) {
+    _classCallCheck(this, DatePicker);
+
+    _get(Object.getPrototypeOf(DatePicker.prototype), 'constructor', this).call(this, props);
 
     this.state = {
-      displayedMonth: props.date.clone().startOf('month')
+      date: (0, _moment2['default'])(props.date),
+      isCalendarOpen: false
     };
   }
 
-  renderWeeks () {
-    const _daysOfMonth = daysOfMonth(this.state.displayedMonth);
-    const _weekEnum = weekEnum(_daysOfMonth);
+  _createClass(DatePicker, [{
+    key: 'componentDidMount ',
+    value: function componentDidMount () {
+      document.addEventListener('mousedown', this.onClickOutside.bind(this));
+      document.addEventListener('touchstart', this.onClickOutside.bind(this));
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      document.removeEventListener('mousedown', this.onClickOutside.bind(this));
+      document.removeEventListener('touchstart', this.onClickOutside.bind(this));
+    }
+  }, {
+    key: 'onClickOutside',
+    value: function onClickOutside(e) {
+      var calendar = _reactDom2['default'].findDOMNode(this.refs.calendar);
+      var dateInput = _reactDom2['default'].findDOMNode(this.refs.dateInput);
 
-    return _weekEnum.map((week, key) => {
-      return (
-        <tr key={key}>
-          {this.renderDay(_daysOfMonth, week)}
-        </tr>
-      );
-    });
-  }
+      if (!calendar) return;
 
-  renderDay (daysOfMonth, week) {
-    const days = [0, 1, 2, 3, 4, 5, 6];
-    const { minDate, maxDate } = this.props;
+      if (!calendar.contains(e.target) && !dateInput.contains(e.target)) {
+        this.setState({
+          isCalendarOpen: false
+        });
+      }
+    }
+  }, {
+    key: 'toggleCalendar',
+    value: function toggleCalendar() {
+      this.setState({
+        isCalendarOpen: !this.state.isCalendarOpen
+      });
+    }
+  }, {
+    key: 'renderCalendar',
+    value: function renderCalendar() {
+      var _props = this.props;
+      var minDate = _props.minDate;
+      var maxDate = _props.maxDate;
+      var calendarClassName = _props.calendarClassName;
+      var dayClassName = _props.dayClassName;
+      var dayActiveClassName = _props.dayActiveClassName;
+      var dayDisabledClassName = _props.dayDisabledClassName;
+      var dayFromOtherMonthClassName = _props.dayFromOtherMonthClassName;
+      var monthClassName = _props.monthClassName;
+      var prevMonthClassName = _props.prevMonthClassName;
+      var nextMonthClassName = _props.nextMonthClassName;
 
-    return days.map((day, key) => {
-      let date = daysOfMonth[week * 7 + day];
-      let disabled;
-
-      if (minDate && maxDate) {
-        disabled = date.isBefore(minDate, 'day') || date.isAfter(maxDate, 'day');
-      } else if (minDate) {
-        disabled = date.isBefore(minDate, 'day');
-      } else if (maxDate) {
-        disabled = date.isAfter(maxDate, 'day');
+      if (!this.state.isCalendarOpen) {
+        return null;
       }
 
-      const dayNextMonth = isDateFromNextMonth(date, this.state.displayedMonth);
-      const dayPrevMonth = isDateFromPrevMonth(date, this.state.displayedMonth);
+      return _react2['default'].createElement(_Calendar2['default'], { ref: 'calendar',
+        date: this.state.date,
+        minDate: minDate,
+        maxDate: maxDate,
+        selectDay: this.selectDay.bind(this),
+        calendarClassName: calendarClassName,
+        dayClassName: dayClassName,
+        dayActiveClassName: dayActiveClassName,
+        dayDisabledClassName: dayDisabledClassName,
+        dayFromOtherMonthClassName: dayFromOtherMonthClassName,
+        monthClassName: monthClassName,
+        prevMonthClassName: prevMonthClassName,
+        nextMonthClassName: nextMonthClassName });
+    }
+  }, {
+    key: 'selectDay',
+    value: function selectDay(date) {
+      var _props2 = this.props;
+      var clickOnDate = _props2.clickOnDate;
+      var name = _props2.name;
 
-      return (
-        <Day key={key}
-             day={date}
-             selectDay={this.selectDay.bind(this, date)}
-             disabled={disabled}
-             dayPrevMonth={dayPrevMonth}
-             dayNextMonth={dayNextMonth}
-             active={this.props.date}
-             dayClassName={this.props.dayClassName}
-             dayActiveClassName={this.props.dayActiveClassName}
-             dayDisabledClassName={this.props.dayDisabledClassName}
-             dayFromOtherMonthClassName={this.props.dayFromOtherMonthClassName} />
+      if (clickOnDate) {
+        this.setState({
+          isCalendarOpen: false
+        });
+        return clickOnDate(date, name);
+      }
+
+      this.setState({
+        isCalendarOpen: false,
+        date: date
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _props3 = this.props;
+      var datepickerClassName = _props3.datepickerClassName;
+      var inputClassName = _props3.inputClassName;
+
+      return _react2['default'].createElement(
+        'div',
+        { className: datepickerClassName },
+        _react2['default'].createElement(_DateInput2['default'], { ref: 'dateInput',
+          inputValue: this.state.date,
+          inputOnClick: this.toggleCalendar.bind(this),
+          inputClassName: inputClassName }),
+        this.renderCalendar()
       );
-    });
-  }
+    }
+  }]);
 
-  renderNameOfDays () {
-    const nameOfDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    return (
-      <tr className='calendar__nameofdays'>
-        {
-          nameOfDays.map((day, i) => <td key={i}>{ day }</td>)
-        }
-      </tr>
-    );
-  }
+  return DatePicker;
+})(_react.Component);
 
-  moveDisplayedMonth (delta) {
-    const dm = this.state.displayedMonth;
-    this.setState({
-      displayedMonth: dm.clone().add(delta, 'months')
-    });
-  }
+exports['default'] = DatePicker;
 
-  selectDay (date) {
-    this.props.selectDay(date);
-  }
-
-  render () {
-    const { calendarClassName, monthClassName, prevMonthClassName, nextMonthClassName } = this.props;
-
-    return (
-      <table className={calendarClassName} cellPadding='0' cellSpacing='0'>
-        <thead>
-          <tr>
-            <td className={prevMonthClassName}
-                onClick={this.moveDisplayedMonth.bind(this, -1)}>
-            </td>
-            <td className={monthClassName} colSpan='5'>
-              {this.state.displayedMonth.format('MMMM YYYY')}
-            </td>
-            <td className={nextMonthClassName}
-                onClick={this.moveDisplayedMonth.bind(this, 1)}>
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          { this.renderNameOfDays() }
-          { this.renderWeeks() }
-        </tbody>
-      </table>
-    );
-  }
-}
-
-Calendar.propTypes = {
-  date: PropTypes.oneOfType([
-    PropTypes.instanceOf(moment),
-    PropTypes.instanceOf(Date)
-  ]),
-  minDate: PropTypes.oneOfType([
-    PropTypes.instanceOf(moment),
-    PropTypes.instanceOf(Date)
-  ]),
-  maxDate: PropTypes.oneOfType([
-    PropTypes.instanceOf(moment),
-    PropTypes.instanceOf(Date)
-  ]),
+DatePicker.propTypes = {
+  date: PropTypes.oneOfType([PropTypes.instanceOf(_moment2['default']), PropTypes.instanceOf(Date)]),
+  minDate: PropTypes.oneOfType([PropTypes.instanceOf(_moment2['default']), PropTypes.instanceOf(Date)]),
+  maxDate: PropTypes.oneOfType([PropTypes.instanceOf(_moment2['default']), PropTypes.instanceOf(Date)]),
+  clickOnDate: PropTypes.func,
+  name: PropTypes.string,
+  datepickerClassName: PropTypes.string,
+  inputClassName: PropTypes.string,
   calendarClassName: PropTypes.string,
-  selectDay: PropTypes.func,
   monthClassName: PropTypes.string,
   prevMonthClassName: PropTypes.string,
   nextMonthClassName: PropTypes.string,
   dayClassName: PropTypes.string,
   dayActiveClassName: PropTypes.string,
   dayDisabledClassName: PropTypes.string,
-  dayFromOtherMonthClassName: PropTypes.string,
+  dayFromOtherMonthClassName: PropTypes.string
 };
 
-Calendar.defaultProps = {
-  calendarClassName: 'calendar',
-  prevMonthClassName: 'calendar__prevMonth',
-  nextMonthClassName: 'calendar__nextMonth',
-  monthClassName: 'calendar__month'
+DatePicker.defaultProps = {
+  date: new Date(),
+  datepickerClassName: 'datepicker'
 };
+module.exports = exports['default'];
